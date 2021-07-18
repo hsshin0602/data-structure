7d63b0c4b3bf4e65eedc680da32483d38623c1cf
신현식 <hsshin0602@gmail.com>
2021-07-18 17:52:01 +0900

    시간 제한:2초(추가시간 없음)	메모리 제한:128MB 문제 N장의 카드가 있다. 각각의 카드는 차례로 1부터 N까지의 번호가 붙어 있으며, 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태로 순서대로 카드가 놓여 있다. 이제 다음과 같은 동작을 카드가 한 장 남을 때까지 반복하게 된다. 우선, 제일 위에 있는 카드를 바닥에 버린다. 그 다음, 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮긴다. 예를 들어 N=4인 경우를 생각해 보자. 카드는 제일 위에서부터 1234 의 순서로 놓여있다. 1을 버리면 234가 남는다. 여기서 2를 제일 아래로 옮기면 342가 된다. 3을 버리면 42가 되고, 4를 밑으로 옮기면 24가 된다. 마지막으로 2를 버리고 나면, 남는 카드는 4가 된다. N이 주어졌을 때, 제일 마지막에 남게 되는 카드를 구하는 프로그램을 작성하시오.
    입력
    첫째 줄에 정수 N(1 ≤ N ≤ 500,000)이 주어진다.
    출력
    첫째 줄에 남게 되는 카드의 번호를 출력한다.
    예제 입력 1
    6
    예제 출력 1
    4

---
+++ 1. queue/Queue_ex/2164.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+
+class myqueue:
+    def __init__(self):
+        self.queue=deque()
+
+    def put(self,data):
+        self.queue.append(data)
+    
+    def get(self):
+        return self.queue.popleft() if self.qsize()==1 else self.queue.popleft()
+
+    def qsize(self):
+        return len(self.queue)
+
+    def rotate(self):
+        self.queue.rotate(-1)
+
+N=int(sys.stdin.readline().rstrip())
+queue=myqueue()
+
+for i in range(1,N+1):
+    queue.put(i)
+while queue.qsize() > 1:
+    queue.get()
+    queue.rotate()
+
+print(queue.get())
