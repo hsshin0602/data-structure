24e9a04a9046d8f6f96e7aeefe9039e86a5b231e
신현식 <hsshin0602@gmail.com>
2021-07-21 10:53:01 +0900

    백준 1021 회전하는 큐 시간제한: 2초 메모리제한: 128MB 문제 지민이는 N개의 원소를 포함하고 있는 양방향 순환 큐를 가지고 있다. 지민이는 이 큐에서 몇 개의 원소를 뽑아내려고 한다. 지민이는 이 큐에서 다음과 같은 3가지 연산을 수행할 수 있다. 1.첫 번째 원소를 뽑아낸다. 이 연산을 수행하면, 원래 큐의 원소가 a1, ..., ak이었던 것이 a2, ..., ak와 같이 된다. 2.왼쪽으로 한 칸 이동시킨다. 이 연산을 수행하면, a1, ..., ak가 a2, ..., ak, a1이 된다. 3.오른쪽으로 한 칸 이동시킨다. 이 연산을 수행하면, a1, ..., ak가 ak, a1, ..., ak-1이 된다. 큐에 처음에 포함되어 있던 수 N이 주어진다. 그리고 지민이가 뽑아내려고 하는 원소의 위치가 주어진다. (이 위치는 가장 처음 큐에서의 위치이다.) 이때, 그 원소를 주어진 순서대로 뽑아내는데 드는 2번, 3번 연산의 최솟값을 출력하는 프로그램을 작성하시오.
    입력
    첫째 줄에 큐의 크기 N과 뽑아내려고 하는 수의 개수 M이 주어진다. N은 50보다 작거나 같은 자연수이고, M은 N보다 작거나 같은 자연수이다. 둘째 줄에는 지민이가 뽑아내려고 하는 수의 위치가 순서대로 주어진다. 위치는 1보다 크거나 같고, N보다 작거나 같은 자연수이다.
    
    출력
    첫째 줄에 문제의 정답을 출력한다.
    
    예제 입력 1
    10 3
    1 2 3
    예제 출력 1
    0
    예제 입력 2
    10 3
    2 9 5
    예제 출력 2
    8
    예제 입력 3
    32 6
    27 16 30 11 6 23
    예제 출력 3
    59
    예제 입력 4
    10 10
    1 6 3 2 7 9 8 4 10 5
    예제 출력 4
    14

---
+++ 1. queue/Queue_ex/1021.py
@@ -0,0 +1,48 @@
+from collections import deque
+import sys
+
+def put(q, data):
+    return q.append(data)
+
+def get(q):
+    return q.popleft()
+
+def rotate_left(q,n):
+    return q.rotate(-1*n)
+
+def rotate_right(q,n):
+    return q.rotate(1*n)
+
+def front(q):
+    return q[0]
+def qsize(q):
+    return len(q)
+
+q=deque()
+N,M=map(int, sys.stdin.readline().rstrip().split())
+arr=list(map(int,sys.stdin.readline().rstrip().split()))
+for i in range(1,N+1):
+    put(q,i)
+count=0
+for i in range(M):
+    if front(q)==arr[i]:
+            get(q)
+    else:
+        cnt_left=q.index(arr[i])
+        cnt_right=qsize(q)-q.index(arr[i])
+        if cnt_right >= cnt_left:
+            count+=cnt_left
+            rotate_left(q, cnt_left)
+            get(q)               
+        elif cnt_right < cnt_left:
+            count+=cnt_right
+            rotate_right(q, cnt_right)
+            get(q)
+
+       
+            
+
+print(count)
+
+
+
