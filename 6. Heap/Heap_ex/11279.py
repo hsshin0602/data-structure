5ccd2135357f92eef747c699281dd86a82ef2733
신현식 <hsshin0602@gmail.com>
2021-08-10 15:32:00 +0900

    백준 11279 최대힙 시간제한:1초 메모리제한:256MB 문제 널리 잘 알려진 자료구조 중 최대 힙이 있다. 최대 힙을 이용하여 다음과 같은 연산을 지원하는 프로그램을 작성하시오. 배열에 자연수 x를 넣는다. 배열에서 가장 큰 값을 출력하고, 그 값을 배열에서 제거한다. 프로그램은 처음에 비어있는 배열에서 시작하게 된다.
    입력
    첫째 줄에 연산의 개수 N(1 ≤ N ≤ 100,000)이 주어진다. 다음 N개의 줄에는 연산에 대한 정보를 나타내는 정수 x가 주어진다. 만약 x가 자연수라면 배열에 x라는 값을 넣는(추가하는) 연산이고, x가 0이라면 배열에서 가장 큰 값을 출력하고 그 값을 배열에서 제거하는 경우이다. 입력되는 자연수는 231보다 작다.
    
    출력
    입력에서 0이 주어진 회수만큼 답을 출력한다. 만약 배열이 비어 있는 경우인데 가장 큰 값을 출력하라고 한 경우에는 0을 출력하면 된다.
    
    예제 입력 1
    13
    0
    1
    2
    0
    0
    3
    2
    1
    0
    0
    0
    0
    0
    예제 출력 1
    0
    2
    1
    3
    2
    1
    0
    0

---
+++ 6. Heap/Heap_ex/11279.py
@@ -0,0 +1,55 @@
+import sys
+
+class Heap:
+    def __init__(self):
+        self.arr=[None]
+
+    def insert(self, item):
+        self.arr.append(item)
+        idx=len(self.arr)-1
+        while idx >1:
+            if self.arr[idx] > self.arr[idx//2]:
+                self.arr[idx],self.arr[idx//2] = self.arr[idx//2], self.arr[idx]
+                idx=idx//2
+            else:
+                break
+
+    def maxHeapify(self,idx):
+        parent=idx
+        left=idx*2
+        right=idx*2+1
+
+        if left < len(self.arr) and self.arr[parent] < self.arr[left]:
+            parent=left
+        if right < len(self.arr) and self.arr[parent] < self.arr[right]:
+            parent=right
+
+        if parent != idx:
+            self.arr[idx],self.arr[parent] = self.arr[parent],self.arr[idx]
+            self.maxHeapify(parent)
+
+    def popRoot(self):
+        if len(self.arr)==1:
+            return 0
+        else:
+            if len(self.arr)==2:
+                ret=self.arr.pop()
+                return ret
+            else:
+                ret=self.arr[1]
+                self.arr[1]=self.arr.pop()
+                self.maxHeapify(1)
+                return ret
+
+case=int(sys.stdin.readline())
+heap=Heap()
+for i in range(case):
+    num=int(sys.stdin.readline())
+    if num == 0:
+        result=heap.popRoot()
+        print(result)
+    else:
+        heap.insert(num)
+        
+
+
